629c1224f23830edab0db3e9a9e14527582cdfbb
mahmoudelgendy <mahmoudelgendy1@gmail.com>
2025-01-05 14:22:53 -0600

    end of chapter 5

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,26 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(String Name, List<String> Categoris, string Description, String ImageFile, decimal Price)
-         : IRequest<CreateProductResult>;
+    public record CreateProductCommand(String Name, List<String> Categories, string Description, String ImageFile, decimal Price)
+         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //ToDo:: Business Logic for creating product
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Categories = command.Categories,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            // ToDo:: save to the database
+
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Register services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // configure pipline
-
+app.MapCarter();
 app.Run();
