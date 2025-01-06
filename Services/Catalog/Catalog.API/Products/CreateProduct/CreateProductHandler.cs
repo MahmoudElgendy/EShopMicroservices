932195dee5ac9930c66750efb0ee01f62facb256
mahmoudelgendy <mahmoudelgendy1@gmail.com>
2025-01-05 23:48:34 -0600

    untill lesson 64

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,9 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(String Name, List<String> Categories, string Description, String ImageFile, decimal Price)
          : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -19,8 +16,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price,
             };
             // ToDo:: save to the database
+            session.Store(product);
+            await session.SaveChangesAsync();
 
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
